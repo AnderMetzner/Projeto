5f16472e65f2afd1c2da2cc3181e3b4655d8b689
Anderson <andemetzner@gmail.com>
2020-10-08 20:52:12 -0300

    tabelas criadas

--- database/migrations/2020_10_08_234823_noticias.php
+++ database/migrations/2020_10_08_234823_noticias.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Noticias extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('noticias', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('titulo');
+            $table->string('conteudo');
+            $table->string('referencias');
+            $table->string('fonte');
+            $table->string('placares');
+            $table->dateTime('data_da_publicacao');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('noticias');
     }
 }
